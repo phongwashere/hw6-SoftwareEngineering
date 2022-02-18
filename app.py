e527d4d45111783c64a26b747adbead55c91d3c2
Phong Vu <phovu265@gmail.com>
2022-02-18 16:06:35 -0500

    seperated add and delete functions into two different app routes, and added redirects to complete the assignment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from ast import IsNot
 import os
 import flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,31 +13,43 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class FavMovies(db.model):
+class FavMovies(db.Model):
     """ creating database """
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String, nullable = False)
+    title = db.Column(db.String(50), nullable = False)
 
 db.create_all()
 
 @app.route("/", methods = ["GET","POST"])
 def index():
-    """ creating index function """
-    if flask.request.method == "POST":
-        data = flask.request.form
-        new_movie = FavMovies(
-            title=data["title"],
-        )
-        db.session.add(new_movie)
-        db.session.commit()
-
-    if flask.request.method == "GET":
-        data = flask.request.form
-        
     movies = FavMovies.query.all()
     num_movies = len(movies)
     return flask.render_template("index.html", num_movies=num_movies, movies=movies)
 
+@app.route("/added", methods = ["GET","POST"])
+def index2():
+    """ creating index function """
+    if flask.request.method == "POST":
+        data = flask.request.form
+        new_movie = FavMovies(title=data["title"])
+        deletion = FavMovies.query.filter_by(title=data["title"]).first()
+        if FavMovies.query.filter_by(title=data["title"]).first() is None:
+            db.session.add(new_movie)
+            db.session.commit()
+        return flask.redirect("/")
+
+@app.route("/deleted", methods = ["GET","POST"])
+def index3():
+    """ creating index function """
+    if flask.request.method == "POST":
+        data = flask.request.form
+        new_movie = FavMovies(title=data["title"])
+        deletion = FavMovies.query.filter_by(title=data["title"]).first()
+        if FavMovies.query.filter_by(title=data["title"]).first() is not None:
+            db.session.delete(deletion)
+            db.session.commit()
+        return flask.redirect("/")
+        
 app.run(
     debug = True
 )
